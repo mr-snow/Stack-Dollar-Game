e91704519853991381682b31f7ff25dab1ab0145
Muhammed Anshif <Muhammadanshif0@gmail.com>
2025-09-18 17:51:15 +0530

    pic add gitdeploy prefic

--- src/Components/user/user.jsx
+++ src/Components/user/user.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import './user.css'
+import './user.css';
 
 function User(props) {
   return (
     <div
       style={{
-        top:props.value.y,
-        left:props.value.x,
+        top: props.value.y,
+        left: props.value.x,
         height: props.value.user_Circle,
         width: props.value.user_Circle,
         backgroundImage: 'url("/Stack-Dollar-Game/user.png")',
-        backgroundSize:'cover',
-        borderRadius:'50%',
-        position:'absolute',
-        zIndex:'3'
+        backgroundSize: 'cover',
+        borderRadius: '50%',
+        position: 'absolute',
+        zIndex: '3',
       }}
-       className='userIcon'
+      className="userIcon"
     ></div>
   );
 }
