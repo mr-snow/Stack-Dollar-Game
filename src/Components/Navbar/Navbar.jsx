19f5cf21cd5e8cd6694fe161c306ab7b5806f006
Muhammed Anshif <Muhammadanshif0@gmail.com>
2025-09-19 16:53:24 +0530

    somes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -19,7 +19,7 @@ function Navbar() {
     <>
       <nav>
         <img src="src/assets/levels/user.png" alt="" />
-          <h2>Sack Dollar3</h2>
+          <h2>Sack Dollar4</h2>
           <i className="fa-solid fa-sack-dollar fa-shake"></i>
           <div className="btns">
             <Button ghost onClick={goHome}>
